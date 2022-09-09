c5cd89d085c6de0f2cd6380bc12e5b72c6e20461
shahikanth888 <shashi.ks172@gmail.com>
2022-09-08 23:24:51 -0400

    Designed Recipes page and shopping page

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'shopping-app';
+  loadedfeature = 'recipe';
+  onNavigate(feature: string) {
+    this.loadedfeature = feature;
+  }
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 2)
+  ];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addIngredient(ing: Ingredient) {
+    this.ingredients.push(ing);
+  }
+
+}
